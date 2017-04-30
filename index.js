1f42bab3d37ccbca583578941892ac558c7d4fe6
UtkinG <utking@mail.ru>
2017-04-30 12:41:59 +1000

    Add the Usage mode to show usage examples for a term; Remove the whole config from the main script

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
 const https = require('https');
 const program = require('commander');
-const api_config = require('./config');
+const modes = require('./config/modes');
+const options = require('./config/config');
 
 program
-  .version('1.0.1')
+  .version('1.0.2')
   .usage('<word|phrase> [options]')
   .option('-s, --synonyms', 'Show synonyms for the word')
+  .option('-e, --examples', 'Show usage examples')
   .parse(process.argv);
-
-const options = {
-  api_hosthane: 'od-api.oxforddictionaries.com',
-  api_path: '/api/v1/entries/',
-  api_synonym_appendix: 'synonyms',
-  api_meaning_appendix: 'definitions',
-  api_id: api_config.api_id,
-  api_port: 443,
-  api_key: api_config.api_key,
-  lang: 'en'
-};
 
 if (!program.args.length || program.args[0].length < 1) {
   console.error(new Error('Empty request'));
   process.exit(0);
 }
 
-const synonymsMode = (program.synonyms ? true : false);
+const mode = getMode(program);
 const word = encodeURIComponent(program.args.join(' '));
-
-let appendix = options.api_meaning_appendix;
-
-if (synonymsMode) {
-  appendix = options.api_synonym_appendix;
-}
+const appendix = getAppendix(mode);
 
 const requestHeaders = {
   hostname: options.api_hosthane,
@@ -57,12 +43,20 @@ let req = https.get(requestHeaders, (res) => {
 
     if (!resp.results) {
       console.info(`Info:: ${appendix} for the word '${decodeURIComponent(word)}' was not found`);
+
     } else {
       let items = resp.results.pop().lexicalEntries;
-      if (synonymsMode)
-        showSynonyms(items);
-      else
-        showMeaning(items);
+      switch (mode) {
+        case modes.SYNONYMS:
+          showSynonyms(items);
+          break;
+        case modes.USAGE:
+          showUsage(items);
+          break;
+        default:
+          showMeaning(items);
+      }
+
     }
   });
 
@@ -72,6 +66,17 @@ let req = https.get(requestHeaders, (res) => {
 }).on('error', (e) => {
   console.error('Error:',e);
 });
+
+function showUsage(resp) {
+  console.info('Usage examples:');
+  let index = 0;
+  resp
+    .map(i => i.sentences)
+    .reduce((p, c) => { return p.concat(c)}, [])
+    .map(i => i.text)
+    .forEach((i) => console.log(`  ${++index}: ${i}`))
+  ;
+}
 
 function showMeaning(resp) {
   console.info('Meanings list:');
@@ -122,3 +127,30 @@ function showSynonyms(resp) {
     .forEach((i) => console.log(`  ${++index}: ${i}`))
   ;
 }
+
+function getMode(program) {
+  let mode = modes.GENERAL;
+  if (program.synonyms) {
+    mode = modes.SYNONYMS;
+  } else if (program.examples) {
+    mode = modes.USAGE;
+  }
+  return mode;
+}
+
+function getAppendix(mode) {
+  let appendix = options.api_meaning_appendix;
+
+  switch (mode) {
+    case modes.SYNONYMS:
+      appendix = options.api_synonym_appendix;
+      break;
+    case modes.USAGE:
+      appendix = options.api_usage_appendix;
+      break;
+    default:
+      appendix = options.api_meaning_appendix;
+  }
+  return appendix;
+}
+
